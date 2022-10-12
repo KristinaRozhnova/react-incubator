b4f4adfc1e97c697b90d0bef73d014091a375388
Kristina <99440794+KristinaRozhnova@users.noreply.github.com>
2022-10-12 15:53:11 +0300

    change Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Accordion from './components/Accordion/Accordion';
+
+function hello() {
+    alert ('hello')
+}
+hello();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("App rendering")
+    return (
+        <div>
+            <Title />
+            <Rating />
+            <Accordion />
+            <Rating />
+        </div>
+    );
 }
+
+function Title () {
+    console.log("Title rendering")
+    return(
+        <>
+            This is App component
+        </>
+    )
+}
+
+function Rating() {
+    console.log("Rating rendering")
+    return (
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    );
+}
+
+function Star() {
+    console.log("Star rendering")
+    return (
+        <div>
+            star
+        </div>
+    )
+}
+
 
 export default App;
